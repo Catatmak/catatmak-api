e7c0d43dc3e68576abcbe584bb04e25aa78962ea
rafiudd <rayhanrafiudd@gmail.com>
2023-12-08 00:18:40 +0700

    feat: create api get chart by type

--- bin/modules/reports/handler/api_handler.js
+++ bin/modules/reports/handler/api_handler.js
@@ -31,11 +31,43 @@ async function getFinancialChartByDate(req, res) {
   return sendResponse(await postRequest(validatePayload));
 };
 
+async function getFinancialChartByType(req, res) {
+  const payload = {
+    ...req.query,
+  };
+  const validatePayload = validator.isValidPayload(payload, queryModel.getWithDate);
+
+  const postRequest = async (result) => {
+    if (result.err) {
+      return result;
+    }
+
+    return getUsecase.getFinancialChartByType({...result.data, mongo: req.mongo, auth: req.auth});
+  };
+
+  const sendResponse = async (result) => {
+    if (result.err) {
+      return wrapper.response(res, 'fail', result, result.message);
+    }
+
+    return wrapper.response(res, 'success', result, result.message, 200);
+  };
+  return sendResponse(await postRequest(validatePayload));
+};
+
 const handlers = [
   {
     method: 'GET',
     path: '/financials/charts',
     handler: getFinancialChartByDate,
+    options: {
+      auth: 'auth-catatmak',
+    },
+  },
+  {
+    method: 'GET',
+    path: '/financials/charts/type',
+    handler: getFinancialChartByType,
     options: {
       auth: 'auth-catatmak',
     },
--- bin/modules/reports/usecase/get_usecase.js
+++ bin/modules/reports/usecase/get_usecase.js
@@ -1,3 +1,4 @@
+/* eslint-disable guard-for-in */
 /* eslint-disable max-len */
 /* eslint-disable require-jsdoc */
 const wrapper = require('../../../utils/wrapper');
@@ -93,6 +94,88 @@ class GetClass {
       finalResult.sort((a, b) => new Date(b.x) - new Date(a.x));
 
       return wrapper.data(finalResult, 'success get financials', 200);
+    } catch (error) {
+      console.log(error);
+      return wrapper.data(error, 'Gagal mengambil data buku', 500);
+    }
+  }
+
+  async getFinancialChartByType(payload) {
+    try {
+      const {phone} = payload.auth.credentials;
+      const collection = payload.mongo.db.collection('financials');
+
+      const pipeline = [];
+
+      const startOfMonth = new Date();
+      startOfMonth.setDate(1); // Set to the first day of the month
+      startOfMonth.setHours(0, 1, 0, 0); // Set to 00:01
+
+      const endOfMonth = new Date(startOfMonth);
+      endOfMonth.setMonth(startOfMonth.getMonth() + 1); // Set to the first day of the next month
+      endOfMonth.setHours(0, 0, 0, 0); // Set to 00:00
+
+      pipeline.push(
+          {
+            $match: {
+              phone,
+              created_at: {
+                $gte: startOfMonth,
+                $lt: endOfMonth,
+              },
+            },
+          },
+          {
+            $addFields: {
+              adjustedDate: {
+                $add: ['$created_at', 7 * 60 * 60 * 1000], // Menambahkan 7 jam dalam milidetik
+              },
+            },
+          },
+          {
+            $project: {
+              _id: 0,
+              date: {$dateToString: {format: '%Y-%m-%d', date: '$adjustedDate'}},
+              price: '$price',
+              type: '$type',
+            },
+          },
+      );
+
+      const data = await collection.aggregate(pipeline).toArray();
+
+      if (data.length < 1) {
+        return wrapper.data({
+          status: false,
+          message: 'Chart data not available',
+          data: {
+            id: 'upss',
+            color: 'hsl(150, 70%, 50%)',
+            data: [],
+          },
+        }, 'success get financials', 404);
+      }
+
+      const result = [];
+
+      // Group the data by date and type
+      const groupedData = data.reduce((acc, entry) => {
+        const key = entry.type;
+        acc[key] = acc[key] || {x: entry.type, y: 0, z: 0};
+        acc[key].z += 1; // Increment count
+        acc[key].y += parseInt(decryptDataAES256Cbc(entry.price)); // Add price (assuming it's hexadecimal)
+        return acc;
+      }, {});
+
+      // Convert the grouped data into the desired format
+      for (const key in groupedData) {
+        result.push(groupedData[key]);
+      }
+
+      // return result;
+
+
+      return wrapper.data(result, 'success get financials', 200);
     } catch (error) {
       console.log(error);
       return wrapper.data(error, 'Gagal mengambil data buku', 500);
