90ec8a838aaddce1da71ad838d7d59c9f70a4a29
rafiudd <rayhanrafiudd@gmail.com>
2023-12-20 14:28:44 +0700

    fix: showing insight if data > 12

--- bin/modules/insight/usecase/get_usecase.js
+++ bin/modules/insight/usecase/get_usecase.js
@@ -33,47 +33,51 @@ class GetClass {
         return wrapper.data(response, 'success get total uncategorize', 200);
       }
 
-      data.map((item) => {
-        response.push({
-          title: decryptDataAES256Cbc(item.title),
-          price: decryptDataAES256Cbc(item.price),
-          type: item.type,
-          category: item.category ?? '',
-          created_at: item.created_at,
-          updated_at: item.updated_at,
+      // just give insight if data > 12
+      if (data.length > 12) {
+        data.map((item) => {
+          response.push({
+            title: decryptDataAES256Cbc(item.title),
+            price: decryptDataAES256Cbc(item.price),
+            type: item.type,
+            category: item.category ?? '',
+            created_at: item.created_at,
+            updated_at: item.updated_at,
+          });
         });
-      });
 
-      // fetch to ml api
-      const config = {
-        method: 'post',
-        url: 'https://catatmak-ml-api-lbiuaop2oq-et.a.run.app/insight',
-        data: response,
-      };
+        // fetch to ml api
+        const config = {
+          method: 'post',
+          url: 'https://catatmak-ml-api-lbiuaop2oq-et.a.run.app/insight',
+          data: response,
+        };
 
-      const mlData = await axios.request(config);
+        const mlData = await axios.request(config);
 
-      console.log(config, mlData);
+        console.log(config, mlData);
 
-      const responseData = mlData.data;
-      if (!responseData) {
-        return wrapper.data([], 'success get insight', 200);
+        const responseData = mlData.data;
+        if (!responseData) {
+          return wrapper.data([], 'success get insight', 200);
+        }
+
+        const insights = [
+          helpers.wordingTotalExpensesInWeek(startOfLastWeekFormatted, endOfLastWeekFormatted, helpers.formatToRupiah(responseData.total_expenses_last_week)),
+          helpers.wordingMostCategories(responseData.most_expensive_category, helpers.formatToRupiah(responseData.total_expenses_most_category)),
+          helpers.wordingDayMostExpenses(responseData.date_most_expenses, helpers.formatToRupiah(responseData.total_expenses_on_day_most_expenses)),
+          helpers.wordingPrediction(responseData.conclusion, helpers.formatToRupiah(responseData.total_predicted_expenses)),
+        ].filter((n) => n);
+
+        const responses = helpers.shuffle(insights);
+
+        if (responses.length < 1 || insights.length < 1) {
+          responses = [];
+        }
+        return wrapper.data(responses, 'success get insight', 200);
       }
 
-      const insights = [
-        helpers.wordingTotalExpensesInWeek(startOfLastWeekFormatted, endOfLastWeekFormatted, helpers.formatToRupiah(responseData.total_expenses_last_week)),
-        helpers.wordingMostCategories(responseData.most_expensive_category, helpers.formatToRupiah(responseData.total_expenses_most_category)),
-        helpers.wordingDayMostExpenses(responseData.date_most_expenses, helpers.formatToRupiah(responseData.total_expenses_on_day_most_expenses)),
-        helpers.wordingPrediction(responseData.conclusion, helpers.formatToRupiah(responseData.total_predicted_expenses)),
-      ].filter((n) => n);
-
-      const responses = helpers.shuffle(insights);
-
-      if (responses.length < 1 || insights.length < 1) {
-        responses = [];
-      }
-
-      return wrapper.data(responses, 'success get insight', 200);
+      return wrapper.data([], 'success get insight', 200);
     } catch (error) {
       // console.log(error);
       return wrapper.data([], 'failed get insight', 500);
